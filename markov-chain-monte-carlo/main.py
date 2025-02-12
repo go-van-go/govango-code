f0d9226f45d9197733fc3f6a4748cbbc50719028
go-van-go <lukejbodmer@gmail.com>
2025-02-12 16:14:42 -0500

    removed some files

--- markov-chain-monte-carlo/main.py
+++ markov-chain-monte-carlo/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.stats import mode
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 import os
 
 def find_spring_params():
@@ -11,8 +12,12 @@ def find_spring_params():
     """
     
     # Parameters
-    m, k, c = 2.0, 4.10, 0.30
-    n = 15000  # Number of runs 
+    n = 15000       # Number of runs 
+    m = 2.0         # mass
+    k = 4.10        # true spring constant
+    c = 0.50        # true damping factor
+    k_guess = 4.30     # spring constant guess
+    c_guess = 0.45  # damping factor guess
     
     # Setup timestepping 
     t_max = 10
@@ -26,7 +31,8 @@ def find_spring_params():
     
     # Create synthetic data
     s = 0.1  # Noise level
-    w = 2 * s  # Random walk step size
+    walk_size_k = 4 * s  # Random walk step size
+    walk_size_c = 0.2 * s  # Random walk step size
     num_samples = 50
     # sample 50 time points
     time_samples = np.linspace(1, num_t_steps-1, num_samples, dtype=int)
@@ -38,26 +44,22 @@ def find_spring_params():
     # add Gaussian noise to create synthetic data
     y_synthetic = y_true_sampled + s * np.random.randn(num_samples)
    
-    # Compute initial likelihood
-    initial_likelihood = np.sum((y_synthetic - y_true_sampled) ** 2)
-    
-    # initialze arrays to keep track of likelihood and
-    # spring parameter guesses
+    # initialze arrays to keep track of likelihood
     likelihood = np.zeros(n)
-    inputs = np.zeros((2, n))
-    inputs[:, 0] = [k, c]
+    initial_likelihood = calculate_likelihood(y_synthetic, y_true_sampled, s)
+    likelihood[0] = initial_likelihood
     old_likelihood = initial_likelihood
-    likelihood[0] = old_likelihood
-    
-    # initial guess for spring parameters
-    k_guess, c_guess = 3, 0.40
-    
+
+    # array of spring parameter guesses
+    inputs = np.zeros((2, n))
+    inputs[:, 0] = [k_guess, c_guess]
+
     # Run Metropolis-Hastings iteration
     for current_run in range(1, n):
         # propose next guesses for k and c
-        kp = abs(k_guess + w * (2 * np.random.rand() - 1))
-        cp = abs(c_guess + w * 0.1 * (2 * np.random.rand() - 1))
-        
+        kp = abs(k_guess + walk_size_k * (2 * np.random.rand() - 1))
+        cp = abs(c_guess + walk_size_c * (2 * np.random.rand() - 1))
+
         # solve simulation with proposed values for k and c
         y_initial = 2.0
         y_dot_initial = 0.0
@@ -70,7 +72,8 @@ def find_spring_params():
         y_proposed_sampled = y_proposed[time_samples]
 
         # calculate the likelihood of the new data
-        new_likelihood = np.sum((y_synthetic - y_proposed_sampled) ** 2)
+        new_likelihood = calculate_likelihood(y_synthetic, y_proposed_sampled, s)
+
         ratio = np.exp(-(new_likelihood - old_likelihood) / 2)
         # rejection criteria 
         acceptance_ratio = min(1, ratio)
@@ -79,17 +82,24 @@ def find_spring_params():
         if np.random.rand() < acceptance_ratio:
             k_guess, c_guess = kp, cp
             old_likelihood = new_likelihood
-        
+
         # update guesses and likelihood
         inputs[:, current_run] = [k_guess, c_guess]
         likelihood[current_run] = old_likelihood
 
-        #plot_data(current_run, inputs, y_true, y_proposed, t, m)
+        if current_run < 200:
+            plot_data(current_run, inputs, y_true, y_synthetic, y_proposed, t, time_of_samples, m, current_run)
+        elif current_run < 2000 and current_run%5==0:
+            plot_data(current_run, inputs, y_true, y_synthetic, y_proposed, t, time_of_samples, m, current_run)
+        elif current_run > 2000 and current_run%10==0:
+            plot_data(current_run, inputs, y_true, y_synthetic, y_proposed, t, time_of_samples, m, current_run)
 
     plot_k_vs_c(inputs)
     plot_histograms(inputs)
     plot_best(inputs, y_true, k_guess, c_guess, m, t, time_of_samples, y_synthetic)
 
+def calculate_likelihood(y_synthetic, y_proposed, s):
+    return np.sum((y_synthetic - y_proposed) ** 2)
 
 def damped_oscillator_solver(m, k, c, y_initial, y_dot_initial, dt, num_t_steps):
     # initialize y and y dot
@@ -147,7 +157,6 @@ def plot_histograms(inputs):
 
        
 def plot_best(inputs, y_true, k_guess, c_guess, m, t, time_of_samples, y_synthetic):
-    csfont = {'fontname':'Comic Sans MS'}
     plt.figure(figsize=(10, 8))
    
     # best guess
@@ -167,7 +176,7 @@ def plot_best(inputs, y_true, k_guess, c_guess, m, t, time_of_samples, y_synthet
     plt.plot(t, y_first, 'g', label='Initial Estimate')
     plt.scatter(time_of_samples, y_synthetic, color='b', marker='o', label='Noisy Measurements')
     plt.legend()
-    plt.title('Current y_proposed vs yTrue', **csfont)
+    plt.title('Current y_proposed vs yTrue')
    
     # Save the figure
     plt.tight_layout()
@@ -176,68 +185,71 @@ def plot_best(inputs, y_true, k_guess, c_guess, m, t, time_of_samples, y_synthet
     plt.close()
 
 
-def plot_data(current_run, inputs, y_true, y_proposed, t, m):
-    # --- Plotting at each iteration ---
+def plot_data(current_run, inputs, y_true, y_synthetic, y_proposed, t, time_of_samples, m, n):
+    # --- Plotting settings ---
     edge_color = "#eceff4"
     font_color = "#eceff4"
     bg_color = "#4c566a"
     axis_color = "#eceff4"
     border_color = "#eceff4"
     bins = 50
-    plt.figure(figsize=(10, 8), facecolor=bg_color)
     
+    # Create figure and subplots
+    fig, axs = plt.subplots(2, 2, figsize=(12, 8), facecolor=bg_color)
+    fig.patch.set_facecolor(bg_color)
+
+    # Add a supertitle for the figure
+    fig.suptitle(f"Run {n}", x=0.511, color=font_color, fontsize=16)
+    
+    # Apply common settings to every subplot
+    for ax in axs.flat:
+        ax.tick_params(axis='both', colors=axis_color)
+        for spine in ax.spines.values():
+            spine.set_color(border_color)
+        ax.set_facecolor(bg_color)
+        ax.xaxis.label.set_color(font_color)
+        ax.yaxis.label.set_color(font_color)
+        ax.title.set_color(font_color)
+
     # Scatter plot of k vs. c
-    plt.subplot(2, 2, 1, facecolor=bg_color)
-    plt.scatter(inputs[0, :current_run], inputs[1, :current_run], color=font_color, alpha=0.3)
-    plt.xlabel('k', color=font_color)
-    plt.ylabel('c', color=font_color)
-    plt.title('Scatter plot of k vs. c', color=font_color)
-    plt.tick_params(axis='both', colors=axis_color)
-    plt.gca().spines['top'].set_color(border_color)
-    plt.gca().spines['right'].set_color(border_color)
-    plt.gca().spines['left'].set_color(border_color)
-    plt.gca().spines['bottom'].set_color(border_color)
+    axs[0, 1].scatter(inputs[0, :current_run], inputs[1, :current_run], color=font_color, alpha=0.2)
+    axs[0, 1].set_xlabel('k', color=font_color)
+    axs[0, 1].set_ylabel('c', color=font_color)
+    axs[0, 1].set_title('Scatter plot of k vs. c', color=font_color)
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    
     
     # y_proposed vs yTrue
-    plt.subplot(2, 2, 2, facecolor=bg_color)
-    plt.plot(t, y_true, label='True Solution', color="#b48ead")
-    plt.plot(t, y_proposed, label='Current Guess', color="#ebcb8b")
-    plt.legend(facecolor=bg_color, labelcolor=font_color, loc="upper right")
-    plt.title('Current y_proposed vs y_true', color=font_color)
-    plt.ylim(-2.0, 2.0)
-    plt.tick_params(axis='both', colors=axis_color)
-    plt.gca().spines['top'].set_color(border_color)
-    plt.gca().spines['right'].set_color(border_color)
-    plt.gca().spines['left'].set_color(border_color)
-    plt.gca().spines['bottom'].set_color(border_color)
-    
+    axs[1, 0].plot(t, y_true, label='True Solution', color="#b48ead")
+    axs[1, 0].plot(t, y_proposed, label='Current Guess', color="#ebcb8b")
+    axs[1, 0].scatter(time_of_samples, y_synthetic, color="#b48ead", marker='.', label='Measurements')
+    axs[1, 0].legend(facecolor=bg_color, labelcolor=font_color, loc="upper right")
+    axs[1, 0].set_title('Current y_proposed vs y_true', color=font_color)
+    axs[1, 0].set_xlabel('time', color=font_color)
+    axs[1, 0].set_ylim(-2.1, 2.5)
+    axs[1, 0].xaxis.set_major_formatter(FormatStrFormatter('%.0f'))
+    axs[1, 0].yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+   
     # Histogram of k values
-    plt.subplot(2, 2, 3, facecolor=bg_color)
-    plt.hist(inputs[0, :current_run], bins=bins, color="#5e81ac", edgecolor=edge_color)
-    plt.xlabel('k values', color=font_color)
-    plt.title('Histogram of k', color=font_color)
-    plt.tick_params(axis='both', colors=axis_color)
-    plt.gca().spines['top'].set_color(border_color)
-    plt.gca().spines['right'].set_color(border_color)
-    plt.gca().spines['left'].set_color(border_color)
-    plt.gca().spines['bottom'].set_color(border_color)
-    
+    axs[1, 1].hist(inputs[0, :current_run], bins=bins, color="#5e81ac", edgecolor=edge_color)
+    axs[1, 1].set_xlabel('k values', color=font_color)
+    axs[1, 1].set_title('Histogram of k', color=font_color)
+    axs[1, 1].xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    axs[1, 1].yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
+
     # Histogram of c values
-    plt.subplot(2, 2, 4, facecolor=bg_color)
-    plt.hist(inputs[1, :current_run], bins=bins, color="#bf616a", edgecolor=edge_color)
-    plt.xlabel('c values', color=font_color)
-    plt.title('Histogram of c', color=font_color)
-    plt.tick_params(axis='both', colors=axis_color)
-    plt.gca().spines['top'].set_color(border_color)
-    plt.gca().spines['right'].set_color(border_color)
-    plt.gca().spines['left'].set_color(border_color)
-    plt.gca().spines['bottom'].set_color(border_color)
- 
+    axs[0, 0].hist(inputs[1, :current_run], bins=bins, orientation="horizontal", color="#bf616a", edgecolor=edge_color)
+    axs[0, 0].set_ylabel('c values', color=font_color)
+    axs[0, 0].set_title('Histogram of c', color=font_color)
+    axs[0, 0].xaxis.set_major_formatter(FormatStrFormatter('%.0f'))
+    axs[0, 0].yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+  
     # Save the figure
-    plt.tight_layout()
-    plt.savefig(f'mcmc_images/frame_{current_run:04d}.png')
+    plt.subplots_adjust(hspace=0.3)
+    plt.savefig(f'mcmc_images/frame_{current_run:06d}.png')
     plt.close()
 
-
+    
 # Run the function
 find_spring_params()
